2ad11612af564fd068bdf4f0dd240f3dbc97f12c
OllieB <ollie@myrtlesoftware.com>
2018-08-31 12:27:53 +0100

    Add editor config and auto vim plugin installation

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 # Find dotfile repo location
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-ln -s $DIR/.vimrc ~/.vimrc
+ln -is $DIR/.vimrc ~/.vimrc
+ln -is $DIR/.editorconfig ~/.editorconfig
 
+echo "Installing all vim plugins"
+vim +'PlugInstall --sync' +qa
